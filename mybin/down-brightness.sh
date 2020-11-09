a45e0409ddba54e058a5276e945a14e4b9e884dd
Suson Waiba <susonwaiba@gmail.com>
2020-11-09 17:53:32 +0545

    updated brightness up & down 😀

--- mybin/down-brightness.sh
+++ mybin/down-brightness.sh
@@ -2,10 +2,14 @@
 
 CURRENT_BRIGHTNESS=$(light -s sysfs/backlight/intel_backlight -G)
 
-# decrease brightness by 5% but don't go below 5%
+# decrease brightness but don't go below 1%
 # -s path can be obtain from light -L
-if [[ 1 -eq $(echo "${CURRENT_BRIGHTNESS} > 5.00" | bc) ]]
+if [[ 1 -eq $(echo "${CURRENT_BRIGHTNESS} > 10.00" | bc) ]]
 then
 	light -s sysfs/backlight/intel_backlight -U 5
 	echo "$(~/mybin/show-brightness.sh)"
+elif [[ 1 -eq $(echo "${CURRENT_BRIGHTNESS} > 1.00" | bc) ]]
+then
+	light -s sysfs/backlight/intel_backlight -U 1
+	echo "$(~/mybin/show-brightness.sh)"	
 fi
--- mybin/up-brightness.sh
+++ mybin/up-brightness.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# increase brightness by 5%
+CURRENT_BRIGHTNESS=$(light -s sysfs/backlight/intel_backlight -G)
+
+# increase brightness
 # -s path can be obtain from light -L
-light -s sysfs/backlight/intel_backlight -A 5
-echo "$(~/mybin/show-brightness.sh)"
+if [[ 1 -eq $(echo "${CURRENT_BRIGHTNESS} < 10.00" | bc) ]]
+then
+    light -s sysfs/backlight/intel_backlight -A 1
+	echo "$(~/mybin/show-brightness.sh)"	
+else
+    light -s sysfs/backlight/intel_backlight -A 5
+    echo "$(~/mybin/show-brightness.sh)"
+fi
